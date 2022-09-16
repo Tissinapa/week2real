1fe496d14a577889bb4fc37f3a11786e96f11b6b
Akseli Hulkkonen <akseli.hulkkonen@gmail.com>
2022-09-16 11:48:11 +0300

    Task 3 done

--- src/index.js
+++ src/index.js
@@ -22,20 +22,20 @@ function initializeCode() {
         let emailColumn = row.insertCell(1);
         let addressColumn = row.insertCell(2);
         let adminColumn = row.insertCell(3);
+        /*let imageColumn = row.insertCell(4);*/
 
-        /*console.log("toimii");*/
+       
 
         userColumn.innerHTML = document.getElementById("input-username").value;
         emailColumn.innerHTML = document.getElementById("input-email").value;
         addressColumn.innerHTML = document.getElementById("input-address").value;
         adminColumn.innerHTML = checkboxAdmin();
+        /*imageColumn.innerHTML = displayImage();*/
         
     })
 
     btnRemoveTable.addEventListener("click", function(){
-        const table=document.getElementById("dataTable");
-        /*const rows=document.getElementsByTagName("tr");*/
-        
+        const table=document.getElementById("dataTable");      
         for(let i=table.rows.length -1 ; i> 0 ; i--){
             table.deleteRow(i);
         } 
@@ -51,7 +51,6 @@ function countRowsInTable(){
         rows=rows+1;
     }
     
-    /*console.log(rows);*/
 }
 function checkboxAdmin(){
     const admin = document.getElementById("input-admin");
@@ -61,3 +60,14 @@ function checkboxAdmin(){
         return "-"
     }
 }
+/*function displayImage(){
+    const img = document.createElement("img");
+    const userImage = document.getElementById("input-image").files[0];
+    img.src = URL.createObjectURL(userImage);
+    img.height = 64;
+    img.width = 64;
+    userImage.appendChild(img);
+    
+
+
+}*/
